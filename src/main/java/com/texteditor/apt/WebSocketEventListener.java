75613e5c92edecf91a81e9a45fb446c6b7878b31
Osama <osamatamer101@gmail.com>
2026-04-15 02:26:47 +0200

    (2) WebSocket Server

--- src/main/java/com/texteditor/apt/WebSocketEventListener.java
+++ src/main/java/com/texteditor/apt/WebSocketEventListener.java
@@ -6,10 +6,6 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 @Component
 public class WebSocketEventListener {
-
-    // Catches ungraceful disconnects (browser crash, network drop, etc.)
-    // The client should send /leave before closing when possible,
-    // but this is the safety net for when they can't.
     @EventListener
     public void handleDisconnect(SessionDisconnectEvent event) {
         System.out.println("[WebSocketEventListener] Session disconnected: " + event.getSessionId());
